a892c8ad8e2e85260ecd633ff9aed2224b22e0e3
Brian Merrell <brian@merrells.org>
2011-01-13 13:47:17 -0500

    Fixed issue in ClientTransport.execute where request parameters were not be urlencoded into the uri

--- pyes/connection_http.py
+++ pyes/connection_http.py
@@ -10,6 +10,7 @@ import logging
 import random
 import threading
 import time
+import urllib
 from pyes.exceptions import NoServerAvailable
 import urllib3
 from httplib import HTTPConnection
@@ -51,7 +52,10 @@ class ClientTransport(object):
         """
         Execute a request and return a response
         """
-        response = self.client.urlopen(Method._VALUES_TO_NAMES[request.method], request.uri, body=request.body, headers=request.headers)    
+        uri = request.uri
+        if request.parameters:
+            uri += '?' + urllib.urlencode(request.parameters)
+        response = self.client.urlopen(Method._VALUES_TO_NAMES[request.method], uri, body=request.body, headers=request.headers)    
         return RestResponse(status=response.status, body=response.data, headers=response.headers)
 
 def connect(servers=None, framed_transport=False, timeout=None,

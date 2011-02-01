81cf2085bb43742b722e833f8cec6e65e2906ec0
Richard Boulton <richard@tartarus.org>
2011-02-01 18:05:43 +0000

    Test that various exceptions are correctly converted

--- pyes/tests/errors.py
+++ pyes/tests/errors.py
@@ -12,19 +12,44 @@ class ErrorReportingTestCase(ESTestCase):
         super(ErrorReportingTestCase, self).setUp()
 
     def testCreateDelete(self):
-        """
-
-        Test errors thrown when creating or deleting.
+        """Test errors thrown when creating or deleting indexes.
 
         """
         try:
             self.conn.delete_index("test-index")
         except pyes.exceptions.NotFoundException:
             pass
+
         self.conn.create_index("test-index")
-        self.assertRaises(pyes.exceptions.AlreadyExistsException, self.conn.create_index, "test-index")
+        err = self.checkRaises(pyes.exceptions.AlreadyExistsException,
+                               self.conn.create_index, "test-index")
+        self.assertEqual(str(err), "[test-index] Already exists")
+        self.assertEqual(err.status, 400)
+        self.assertTrue('error' in err.result)
+        self.assertTrue('ok' not in err.result)
+
         self.conn.delete_index("test-index")
-        self.assertRaises(pyes.exceptions.NotFoundException, self.conn.delete_index, "test-index")
+        err = self.checkRaises(pyes.exceptions.NotFoundException,
+                               self.conn.delete_index, "test-index")
+        self.assertEqual(str(err), "[test-index] missing")
+        self.assertEqual(err.status, 400)
+        self.assertTrue('error' in err.result)
+        self.assertTrue('ok' not in err.result)
+
+    def testMissingIndex(self):
+        """Test generation of a IndexMissingException.
+
+        """
+        try:
+            self.conn.delete_index("test-index")
+        except pyes.exceptions.NotFoundException:
+            pass
+        err = self.checkRaises(pyes.exceptions.IndexMissingException,
+                               self.conn.flush, 'test-index')
+        self.assertEqual(str(err), "[test-index] missing")
+        self.assertEqual(err.status, 500)
+        self.assertTrue('error' in err.result)
+        self.assertTrue('ok' not in err.result)
 
 if __name__ == "__main__":
     unittest.main()

f4c0355e83fcfcfc15fecdaa5ad5c83f944bdac5
Alberto Paro <alberto@ingparo.it>
2011-03-18 22:06:50 +0100

    Added ExistsFilter

--- pyes/filters.py
+++ pyes/filters.py
@@ -198,6 +198,11 @@ class TermFilter(Filter):
             raise RuntimeError("A least a field/value pair must be added")
         return {self._internal_name:self._values}
 
+class ExistsFilter(TermFilter):
+    _internal_name = "exists"
+    def __init__(self, field=None, **kwargs):
+        super(ExistsFilter, self).__init__(field="field", value=field, **kwargs)
+
 class MissingFilter(TermFilter):
     _internal_name = "missing"
     def __init__(self, field=None, **kwargs):

cfc4f615c54faebcf1f84a2238a97975919c870a
Alberto Paro <alberto@ingparo.it>
2012-04-19 23:13:25 +0200

    Added script to install es with plugins

---
+++ install_es.sh
@@ -0,0 +1,4 @@
+wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.2.zip
+unzip elasticsearch-0.19.2.zip
+mv elasticsearch-0.19.2 elasticsearch
+elasticsearch/bin/plugin -install lukas-vlcek/bigdesk -install mobz/elasticsearch-head -install elasticsearch/elasticsearch-lang-python/1.1.0 -install elasticsearch/elasticsearch-lang-javascript/1.1.0 -install elasticsearch/elasticsearch-mapper-attachments/1.4.0
--- pyes/es.py
+++ pyes/es.py
@@ -58,7 +58,6 @@ class DotDict(dict):
     def __deepcopy__(self, memo):
         return DotDict([(copy.deepcopy(k, memo), copy.deepcopy(v, memo)) for k, v in self.items()])
 
-
 class ElasticSearchModel(DotDict):
     def __init__(self, *args, **kwargs):
         self._meta = DotDict()

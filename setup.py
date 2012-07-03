48cb94af257ced7daf608c4ac5baadf79ec034fa
George Sakkis <george@podio.com>
2012-07-03 22:19:18 +0200

    Go back to urllib3 instead of requests

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class QuickRunTests(TestCommand):
         TestCommand.run(self, *args, **kwargs)
 
 
-install_requires = ["requests"]
+install_requires = ["urllib3"]
 
 #if not sys.platform.startswith("java"):
 #    install_requires += [ "thrift", ]    

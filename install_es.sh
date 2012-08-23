c57eb39c8bd43939ccb198a8f192ce91acbe200a
Alberto Paro <alberto@ingparo.it>
2012-08-23 22:16:06 +0200

    Upgraded Elastichsearch installer to 0.19.9

--- install_es.sh
+++ install_es.sh
@@ -1,4 +1,4 @@
-wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.4.zip
-unzip elasticsearch-0.19.4.zip
-mv elasticsearch-0.19.4 elasticsearch
+wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.9.zip
+unzip elasticsearch-0.19.9.zip
+mv elasticsearch-0.19.9 elasticsearch
 elasticsearch/bin/plugin -install lukas-vlcek/bigdesk -install mobz/elasticsearch-head -install elasticsearch/elasticsearch-lang-python/1.1.0 -install elasticsearch/elasticsearch-lang-javascript/1.1.0 -install elasticsearch/elasticsearch-mapper-attachments/1.4.0

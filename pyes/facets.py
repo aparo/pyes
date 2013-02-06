43695069537d081dc02b34113d20192b210151b4
George Sakkis <george@podio.com>
2013-02-06 19:58:33 +0100

    Facet refactoring

--- pyes/facets.py
+++ pyes/facets.py
@@ -35,15 +35,17 @@ class FacetFactory(EqualityComparableUsingAttributeDictionary):
 
 
 class Facet(EqualityComparableUsingAttributeDictionary):
-    def __init__(self, scope=None, nested=None,
-                 is_global=None, facet_filter=None, *args, **kwargs):
+
+    def __init__(self, name, scope=None, nested=None, is_global=None,
+                 facet_filter=None, **kwargs):
+        self.name = name
         self.scope = scope
         self.nested = nested
         self.is_global = is_global
         self.facet_filter = facet_filter
 
-    def _base_parameters(self):
-        data = {}
+    def serialize(self):
+        data = {self._internal_name: self._serialize()}
         if self.scope is not None:
             data["scope"] = self.scope
         if self.nested is not None:
@@ -52,7 +54,14 @@ class Facet(EqualityComparableUsingAttributeDictionary):
             data['global'] = self.is_global
         if self.facet_filter:
             data['facet_filter'] = self.facet_filter.serialize()
-        return data
+        return {self.name: data}
+
+    def _serialize(self):
+        raise NotImplementedError
+
+    @property
+    def _internal_name(self):
+        raise NotImplementedError
 
 
 # TODO: remove these
@@ -64,43 +73,37 @@ NotFacetFilter = NotFilter
 
 
 class QueryFacet(Facet):
+
     _internal_name = "query"
 
     def __init__(self, name, query, **kwargs):
-        super(QueryFacet, self).__init__(**kwargs)
-        self.name = name
+        super(QueryFacet, self).__init__(name, **kwargs)
         self.query = query
 
-    def serialize(self):
-        data = self._base_parameters()
-        data[self._internal_name] = self.query.serialize()
-        return {self.name: data}
+    def _serialize(self):
+        return self.query.serialize()
 
 
 class FilterFacet(Facet):
+
     _internal_name = "filter"
 
-    def __init__(self, name, query, **kwargs):
-        super(FilterFacet, self).__init__(**kwargs)
-        self.name = name
-        self.query = query
+    def __init__(self, name, filter, **kwargs):
+        super(FilterFacet, self).__init__(name, **kwargs)
+        self.filter = filter
 
-    def serialize(self):
-        data = self._base_parameters()
-        data[self._internal_name] = self.query.serialize()
-        return {self.name: data}
+    def _serialize(self):
+        return self.filter.serialize()
 
 
 class HistogramFacet(Facet):
+
     _internal_name = "histogram"
 
-    def __init__(self, name,
-                 field=None, interval=None, time_interval=None,
-                 key_field=None, value_field=None,
-                 key_script=None, value_script=None, params=None,
-                 **kwargs):
-        super(HistogramFacet, self).__init__(**kwargs)
-        self.name = name
+    def __init__(self, name, field=None, interval=None, time_interval=None,
+                 key_field=None, value_field=None, key_script=None,
+                 value_script=None, params=None, **kwargs):
+        super(HistogramFacet, self).__init__(name, **kwargs)
         self.field = field
         self.interval = interval
         self.time_interval = time_interval
@@ -118,7 +121,7 @@ class HistogramFacet(Facet):
         else:
             raise RuntimeError("Invalid field: interval or time_interval required")
 
-    def serialize(self):
+    def _serialize(self):
         data = {}
         if self.field:
             data['field'] = self.field
@@ -142,20 +145,16 @@ class HistogramFacet(Facet):
                 data['interval'] = self.interval
             elif self.time_interval:
                 data['time_interval'] = self.time_interval
-        params = self._base_parameters()
-        params[self._internal_name] = data
-        return {self.name: params}
+        return data
 
 
 class DateHistogramFacet(Facet):
+
     _internal_name = "date_histogram"
 
-    def __init__(self, name,
-                 field=None, interval=None, zone=None,
-                 key_field=None, value_field=None,
-                 value_script=None, params=None, **kwargs):
-        super(DateHistogramFacet, self).__init__(**kwargs)
-        self.name = name
+    def __init__(self, name, field=None, interval=None, zone=None, key_field=None,
+                 value_field=None, value_script=None, params=None, **kwargs):
+        super(DateHistogramFacet, self).__init__(name, **kwargs)
         self.field = field
         self.interval = interval
         self.zone = zone
@@ -164,9 +163,8 @@ class DateHistogramFacet(Facet):
         self.value_script = value_script
         self.params = params
 
-    def serialize(self):
+    def _serialize(self):
         data = {}
-
         if self.interval:
             data['interval'] = self.interval
             if self.zone:
@@ -185,39 +183,29 @@ class DateHistogramFacet(Facet):
                     data['params'] = self.params
             else:
                 raise RuntimeError("Invalid key_field: value_field or value_script required")
-
-        facet = self._base_parameters()
-        facet[self._internal_name] = data
-        return {self.name: facet}
+        return data
 
 
 class RangeFacet(Facet):
+
     _internal_name = "range"
 
-    def __init__(self, name,
-                 field=None, ranges=None,
-                 key_field=None, value_field=None,
-                 key_script=None, value_script=None, params=None,
-                 **kwargs):
-        super(RangeFacet, self).__init__(**kwargs)
-        self.name = name
+    def __init__(self, name, field=None, ranges=None, key_field=None, value_field=None,
+                 key_script=None, value_script=None, params=None, **kwargs):
+        super(RangeFacet, self).__init__(name, **kwargs)
         self.field = field
-        if ranges is None:
-            ranges = []
-        self.ranges = ranges
+        self.ranges = ranges or []
         self.key_field = key_field
         self.value_field = value_field
         self.key_script = key_script
         self.value_script = value_script
         self.params = params
 
-    def serialize(self):
+    def _serialize(self):
         data = {}
-
         if not self.ranges:
             raise RuntimeError("Invalid ranges")
         data['ranges'] = self.ranges
-
         if self.field:
             data['field'] = self.field
         elif self.key_field:
@@ -234,42 +222,33 @@ class RangeFacet(Facet):
                 raise RuntimeError("Invalid key_script: value_script required")
             if self.params:
                 data['params'] = self.params
-
-        params = self._base_parameters()
-        params[self._internal_name] = data
-        return {self.name: params}
+        return data
 
 
 class GeoDistanceFacet(RangeFacet):
+
     _internal_name = "geo_distance"
 
-    def __init__(self, name, field, pin,
-                 ranges=None, value_field=None,
-                 value_script=None, distance_unit=None,
-                 distance_type=None, params=None,
-                 **kwargs):
-        super(RangeFacet, self).__init__(**kwargs)
-        self.name = name
+    def __init__(self, name, field, pin, ranges=None, value_field=None,
+                 value_script=None, distance_unit=None, distance_type=None,
+                 params=None, **kwargs):
+        super(RangeFacet, self).__init__(name, **kwargs)
         self.field = field
         self.pin = pin
         self.distance_unit = distance_unit
         self.distance_type = distance_type
-        if ranges is None:
-            ranges = []
-        self.ranges = ranges
+        self.ranges = ranges or []
         self.value_field = value_field
         self.value_script = value_script
         self.params = params
         self.DISTANCE_TYPES = ['arc', 'plane']
         self.UNITS = ['km', 'mi', 'miles']
 
-    def serialize(self):
-        data = {}
-
+    def _serialize(self):
         if not self.ranges:
             raise RuntimeError("Invalid ranges")
+        data = {}
         data['ranges'] = self.ranges
-
         data[self.field] = self.pin
         if self.distance_type:
             if self.distance_type not in self.DISTANCE_TYPES:
@@ -281,56 +260,46 @@ class GeoDistanceFacet(RangeFacet):
                 raise RuntimeError("Invalid unit: must be one of %s" %
                     self.DISTANCE_TYPES)
             data['unit'] = self.distance_unit
-
         if self.value_field:
             data['value_field'] = self.value_field
         elif self.value_script:
             data['value_script'] = self.value_script
             if self.params:
                 data['params'] = self.params
+        return data
 
-        params = self._base_parameters()
-        params[self._internal_name] = data
-        return {self.name: params}
 
 class StatisticalFacet(Facet):
+
     _internal_name = "statistical"
 
     def __init__(self, name, field=None, script=None, params=None, **kwargs):
-        super(StatisticalFacet, self).__init__(**kwargs)
-        self.name = name
+        super(StatisticalFacet, self).__init__(name, **kwargs)
         self.field = field
         self.script = script
         self.params = params
 
-    def serialize(self):
+    def _serialize(self):
         data = {}
-
         if self.field:
             data['field'] = self.field
         elif self.script:
             data['script'] = self.script
             if self.params:
                 data['params'] = self.params
-
-        params = self._base_parameters()
-        params[self._internal_name] = data
-        return {self.name: params}
+        return data
 
 
 class TermFacet(Facet):
+
     _internal_name = "terms"
 
-    def __init__(self, field=None, fields=None, name=None, size=10,
-                 order=None, exclude=None,
-                 regex=None, regex_flags="DOTALL",
-                 script=None, all_terms=None, **kwargs):
-        super(TermFacet, self).__init__(**kwargs)
-        self.name = name
+    def __init__(self, field=None, fields=None, name=None, size=10, order=None,
+                 exclude=None, regex=None, regex_flags="DOTALL", script=None,
+                 all_terms=None, **kwargs):
+        super(TermFacet, self).__init__(name or field, **kwargs)
         self.field = field
         self.fields = fields
-        if name is None:
-            self.name = field
         self.size = size
         self.order = order
         self.exclude = exclude or []
@@ -339,7 +308,7 @@ class TermFacet(Facet):
         self.script = script
         self.all_terms = all_terms
 
-    def serialize(self):
+    def _serialize(self):
         if self.fields:
             data = {'fields': self.fields}
         else:
@@ -347,10 +316,8 @@ class TermFacet(Facet):
                 data = {'field': self.field}
             else:
                 raise RuntimeError("Field or Fields is required:%s" % self.order)
-
         if self.size:
             data['size'] = self.size
-
         if self.order:
             if self.order not in ['count', 'term', 'reverse_count', 'reverse_term']:
                 raise RuntimeError("Invalid order value:%s" % self.order)
@@ -365,20 +332,16 @@ class TermFacet(Facet):
             data['script'] = self.script
         if self.all_terms:
             data['all_terms'] = self.all_terms
-        params = self._base_parameters()
-        params[self._internal_name] = data
-        return {self.name: params}
+        return data
 
 
 class TermStatsFacet(Facet):
+
     _internal_name = "terms_stats"
 
-    def __init__(self, name, size=10, order=None,
-                 key_field=None, value_field=None,
-                 key_script=None, value_script=None, params=None,
-                 **kwargs):
-        super(TermStatsFacet, self).__init__(**kwargs)
-        self.name = name
+    def __init__(self, name, size=10, order=None, key_field=None, value_field=None,
+                 key_script=None, value_script=None, params=None, **kwargs):
+        super(TermStatsFacet, self).__init__(name, **kwargs)
         self.size = size
         self.ORDER_VALUES = ['term', 'reverse_term', 'count', 'reverse_count', 'total',
                              'reverse_total', 'min', 'reverse_min', 'max', 'reverse_max']
@@ -389,17 +352,14 @@ class TermStatsFacet(Facet):
         self.value_script = value_script
         self.params = params
 
-    def serialize(self):
+    def _serialize(self):
         data = {}
-
         if self.size is not None:
             data['size'] = self.size
-
         if self.order:
             if self.order not in self.ORDER_VALUES:
                 raise RuntimeError("Invalid order value:%s" % self.order)
             data['order'] = self.order
-
         if self.key_field:
             data['key_field'] = self.key_field
             if self.value_field:
@@ -414,17 +374,13 @@ class TermStatsFacet(Facet):
                 raise RuntimeError("Invalid key_script: value_script required")
             if self.params:
                 data['params'] = self.params
-
-        params = self._base_parameters()
-        params[self._internal_name] = data
-        return {self.name: params}
+        return data
 
 
 class FacetQueryWrap(EqualityComparableUsingAttributeDictionary):
+
     def __init__(self, wrap_object, **kwargs):
-        """
-        Base Object for every Filter Object
-        """
+        """Base Object for every Filter Object"""
         self.wrap_object = wrap_object
 
     def serialize(self):

5369ec051e9251edd53b8c17149e86b87c5ed6c4
Alberto Paro <alberto.paro@gmail.com>
2013-11-22 13:14:48 +0100

    Updated installer to 0.90.7

--- install_es.sh
+++ install_es.sh
@@ -1,6 +1,6 @@
-wget -c --no-check-certificate https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.tar.gz
-tar xfvz elasticsearch-0.90.5.tar.gz
-mv elasticsearch-0.90.5 elasticsearch
+wget -c --no-check-certificate https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.tar.gz
+tar xfvz elasticsearch-0.90.7.tar.gz
+mv elasticsearch-0.90.7 elasticsearch
 elasticsearch/bin/plugin -install lukas-vlcek/bigdesk -install mobz/elasticsearch-head -install elasticsearch/elasticsearch-lang-python/1.2.0 -install elasticsearch/elasticsearch-lang-javascript/1.3.0 -install elasticsearch/elasticsearch-mapper-attachments/1.7.0 -install elasticsearch/elasticsearch-transport-thrift/1.5.0
 elasticsearch/bin/plugin -install royrusso/elasticsearch-HQ
 elasticsearch/bin/plugin -install elasticsearch/elasticsearch-river-twitter/1.2.0 

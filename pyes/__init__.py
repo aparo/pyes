3fc03f593ae4a28a6bec8e66f54042a83d9c4047
Alberto Paro <alberto.paro@gmail.com>
2014-05-20 13:37:50 +0200

    Released 0.99.2

--- pyes/__init__.py
+++ pyes/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-VERSION = (0, 99, 2, "dev")
+VERSION = (0, 99, 2)
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Alberto Paro"
